3f79dbca3f8edba8d6c3993d7386b0e8f6ca2295
ngoyal95 <nikhil.r.goyal@gmail.com>
2020-03-02 18:41:02 -0500

    added submodule, official ABCD subject measure processing scripts

--- melodic/get_final_sub_list.py
+++ melodic/get_final_sub_list.py
@@ -1,3 +1,6 @@
+# Written by Nikhil Goyal, National Institute of Mental Health, 2019-2020
+# This script is ran after motion_analysis.py, and sm_analysis.py, which determine which subjects to include in the final analysis.
+# This script will then create a txt file of the absolute paths to the NIFTI files we want to feed into MELODIC group-ICA.
 
 import pandas as pd
 import numpy as np
@@ -7,7 +10,7 @@ import sys
 cwd = os.getcwd()
 
 nifti_fp = sys.argv[1]  # path to mat_files.txt
-subs_fp = sys.argv[2]      # path to list of final subjects (in abcd_cca_replication/data/ica_subjects.txt)
+subs_fp = sys.argv[2]   # path to list of final subjects (in abcd_cca_replication/data/motion_filtered_subjects.txt)
 
 NIFTI = [line.rstrip('\n') for line in open(nifti_fp)]
 subs = [line.rstrip('\n') for line in open(subs_fp)]
