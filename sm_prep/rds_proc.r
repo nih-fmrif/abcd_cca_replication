e8905ec795a313c76523dc2ec1def03131245faa
ngoyal95 <nikhil.r.goyal@gmail.com>
2020-03-24 19:45:00 -0400

    update filename

--- sm_prep/rds_proc.r
+++ sm_prep/rds_proc.r
@@ -8,7 +8,7 @@
 library(dplyr)
 
 # Note, you must move the .rds to the working directory of this script!
-nda1 <- readRDS("nda2.0.1.rds")
+nda1 <- readRDS("nda2.0.1.Rds")
 subject_list <- readLines("../motion/data/motion_filtered_subjects_R.txt")
 subject_list <- factor(subject_list)
 
@@ -73,7 +73,7 @@ for (i in 4:NCOL(nda4)) {
 
 nda5 <- nda4[ , !(names(nda4) %in% badcols)]
 
-saveRDS(nda5, "./data/nda2.0.1_full_proc.rds")
+saveRDS(nda5, "./data/nda2.0.1_full_proc.Rds")
 # write.csv(col_inc_excl,"/data/ABCD_MBDU/goyaln2/analysis-nda/notebooks/general/col_inc_excl")
 
 write.table(t(as.data.frame(col_inc_excl)), 
