7be2b1d31988a67bdf213d5e1e34970637e27dd9
ngoyal95 <nikhil.r.goyal@gmail.com>
2020-06-11 15:44:26 -0400

    fixed syntax

--- data_prep/clean_censors.py
+++ data_prep/clean_censors.py
@@ -4,7 +4,7 @@
 # Last edited: 6/11/20
 # Written by Nikhil Goyal, National Institute of Mental Health, 2019-2020
 
-# Argument: list of subjects in format NDARINVxxxxxxx
+# Argument: list of subjects in format NDARINVxxxxxxx (This must be provided on an ABSOLUTE PATH!)
 
 import sys
 import os
@@ -12,10 +12,8 @@ import pandas as pd
 import datetime
 
 # Load data
-filename=sys.argv[1]
+subs_fp=sys.argv[1]
 cwd = os.getcwd()
-subs_fp = os.path.join(cwd,filename+".txt")
-# print(subs_fp)
 
 # format of each line in text file is NDARINVxxxxxxx on each line (need to drop the sub- prefix)
 subs = [line.rstrip('\n') for line in open(subs_fp)]
@@ -25,9 +23,9 @@ failed_subs = []
 # 2. Determine which scans to use, and based on that, cut the needed subset of censoring points from the censor file.
 for sub in subs:
     # Make sure all files are present, otherwise throw an error
-    fp_censor=os.path,join(cwd,"censoring_data/"+sub+"_censor.txt")
-    fp_scan_class=os.path.join(cwd,"data/scan_length_proc/"+sub+"_scans_classified.txt")
-    fp_lens=os.path.join(cwd,"data/scan_length_proc/"+sub+"_scan_lengths.txt")
+    fp_censor       = os.path.join(cwd,"censoring_data/"+sub+"_censor.txt")
+    fp_scan_class   = os.path.join(cwd,"data/scan_length_proc/"+sub+"_scans_classified.txt")
+    fp_lens         = os.path.join(cwd,"data/scan_length_proc/"+sub+"_scan_lengths.txt")
 
     if (os.path.exists(fp_censor) & os.path.exists(fp_scan_class) & os.path.exists(fp_lens)):
         # load their NDARINVA354YMUE_censor.txt file
