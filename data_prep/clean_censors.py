ee68b7a9a2eb43d70d799a852d1fbb6e325a1308
ngoyal95 <nikhil.r.goyal@gmail.com>
2020-06-11 12:57:10 -0400

    finalized scripts for testing

--- data_prep/clean_censors.py
+++ data_prep/clean_censors.py
@@ -4,7 +4,7 @@
 # Last edited: 6/11/20
 # Written by Nikhil Goyal, National Institute of Mental Health, 2019-2020
 
-# Argument: list of subjects in format sub-NDARINVxxxxxxx
+# Argument: list of subjects in format NDARINVxxxxxxx
 
 import sys
 import os
@@ -17,19 +17,17 @@ cwd = os.getcwd()
 subs_fp = os.path.join(cwd,filename+".txt")
 print(subs_fp)
 
-# format of each line in text file is sub-NDARINVxxxxxxx on each line (need to drop the sub- prefix)
+# format of each line in text file is NDARINVxxxxxxx on each line (need to drop the sub- prefix)
 subs = [line.rstrip('\n') for line in open(subs_fp)]
 failed_subs = []
 # For each subject, we need to do the following:
 # 1. Load their NDARINVxxxxxxx_scans_classified.txt and NDARINVxxxxxxx_scan_lengths.txt files
 # 2. Determine which scans to use, and based on that, cut the needed subset of censoring points from the censor file.
 for sub in subs:
-
     # Make sure all files are present, otherwise throw an error
-    sub2=sub.split('sub-')[1]
-    fp_censor=os.path,join(cwd,"censoring_data/"+sub2+"_censor.txt")
-    fp_scan_class=os.path.join(cwd,"censoring_data/"+sub2+"_scans_classified.txt")
-    fp_lens=os.path.join(cwd,"censoring_data/"+sub2+"_scan_lengths.txt")
+    fp_censor=os.path,join(cwd,"censoring_data/"+sub+"_censor.txt")
+    fp_scan_class=os.path.join(cwd,"censoring_data/"+sub+"_scans_classified.txt")
+    fp_lens=os.path.join(cwd,"censoring_data/"+sub+"_scan_lengths.txt")
 
     if (os.path.exists(fp_censor) & os.path.exists(fp_scan_class) & os.path.exists(fp_lens)):
         # load their NDARINVA354YMUE_censor.txt file
@@ -62,7 +60,7 @@ for sub in subs:
                     running_idx=stop_idx
 
             # Save the subsetted censor
-            with open(os.path.join(cwd,"censoring_data/"+sub2+"_censor_subset.txt"), "w") as output:
+            with open(os.path.join(cwd,"censoring_data/"+sub+"_censor_subset.txt"), "w") as output:
                 for item in censor_subset:
                     output.write('%s\n' % item)
 
@@ -78,7 +76,7 @@ for sub in subs:
         continue
 
 if len(failed_subs) > 0:
-    print("Subjects whose censor files couldn't be edited:\n")
+    print("WARNING: Subjects whose censor files couldn't be edited:\n")
     print(failed_subs)
 else:
     print("Done! All subject censors successfully edited.\n")
