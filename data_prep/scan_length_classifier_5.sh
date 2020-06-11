9da33407e4c227c1f1c3cc857d1f2ecfc167415a
ngoyal95 <nikhil.r.goyal@gmail.com>
2020-06-11 13:33:45 -0400

    fixed the find command line in shell script

--- data_prep/scan_length_classifier_5.sh
+++ data_prep/scan_length_classifier_5.sh
@@ -78,7 +78,8 @@ do
 
     # Pull scan lengths from all available scans (format sub-NDARINVFL02R0H4_ses-baselineYear1Arm1_task-rest_run-[0-9][0-9]_bold.nii.gz)
     # Note, values are written to file in ascending order (i.e. scan 1 length on line one, scan 2 on line 2, etc...)
-    find $raw_path/ses-baselineYear1Arm1/func/ -type f -name "*task-rest_run*[0-9][0-9]_bold.nii.gz" | sort | -exec fslnvols {} \; tee -a $DATAFOLDER/timepoints_subs.txt | tee -a $DATAFOLDER/timepoints_no_subs.txt | tee $DATAFOLDER/${sub_id_noprefix}_scan_lengths.txt
+    # find $raw_path/ses-baselineYear1Arm1/func/ -type f -name "*task-rest_run*[0-9][0-9]_bold.nii.gz" | sort | -exec fslnvols {} \; tee -a $DATAFOLDER/timepoints_subs.txt | tee -a $DATAFOLDER/timepoints_no_subs.txt | tee $DATAFOLDER/${sub_id_noprefix}_scan_lengths.txt
+    find $raw_path/ses-baselineYear1Arm1/func/ -type f -name "*task-rest_run*[0-9][0-9]_bold.nii.gz" | sort | xargs  -L 1 fslnvols | tee -a $DATAFOLDER/timepoints_subs.txt | tee -a $DATAFOLDER/timepoints_no_subs.txt | tee $DATAFOLDER/${sub_id_noprefix}_scan_lengths.txt
 
     # Create classifier file for each subject
     count=1
