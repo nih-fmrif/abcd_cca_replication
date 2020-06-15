274e46465e7b6c8ed88475510456e6ccde091edf
ngoyal95 <nikhil.r.goyal@gmail.com>
2020-06-15 18:38:04 -0400

    created new script that generates a config for pipeline, complete prep_stage_1.sh ready for testing

---
+++ create_config.sh
@@ -0,0 +1,69 @@
+#! /bin/bash
+
+# create_config.sh
+# Created: 6/15/20
+# Updated:
+
+usage()
+{
+	echo "usage: create_config.sh <path/to/main/abcd_bids/bids/>"
+    echo "NOTE you must provide the ABSOLUTE PATH to the main directory of the ABCD collection 3165 download. for example: /data/ABCD/abcd_bids/bids/"
+}
+
+if (( $# < 1 ))
+then
+    usage
+	exit 1
+fi
+
+BIDS_PATH=$1
+DERIVATES_PATH=$BIDS_PATH/derivatives/abcd-hcp-pipeline/
+DATA_PREP=$PWD/data_prep/
+REPO_DATA_FOLDER=$PWD/data/
+PIPELINE_LOG_DIR=$DATA_PREP/logs/
+PREP_LOG=$DATA_PREP/logs/prep_log.txt
+SUPPORT_SCRIPTS=$DATA_PREP/support_scripts/
+CENSOR_FILES=$PWD/data_prep/data/censor_files/
+CONFIG=$PWD/pipeline.config
+
+echo "---ABCD CCA Pipeline Config Setup---"
+echo "BIDS_PATH=$BIDS_PATH"
+echo "DERIVATIVES_PATH=$DERIVATES_PATH"
+echo "DATA_PREP=$DATA_PREP"
+echo "REPO_DATA_FOLDER=$REPO_DATA_FOLDER"
+echo "PIPELINE_LOG_DIR=$PIPELINE_LOG_DIR"
+echo "PREP_LOG=$PREP_LOG"
+echo "SUPPORT_SCRIPTS=$SUPPORT_SCRIPTS"
+echo "CENSOR_FILES=$CENSOR_FILES"
+echo "CONFIG=$CONFIG"
+echo "------------------------------------"
+
+if [[ -f $CONFIG ]]; then
+    read -p "A config file already exists. Are you sure you want to overwrite it? " -n 1 -r
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        # overwrite it
+        echo "Configuration file overwritten."
+        echo
+        rm $CONFIG
+        touch $CONFIG
+    else
+        # Exit
+        echo "NOT overwriting config file."
+        exit 1
+    fi
+else
+    # config doesn't exist, so create it
+    touch $CONFIG
+fi
+
+# Now write these variables
+echo "BIDS_PATH=$BIDS_PATH" >> $CONFIG
+echo "DERIVATIVES_PATH=$DERIVATES_PATH" >> $CONFIG
+echo "DATA_PREP=$DATA_PREP" >> $CONFIG
+echo "REPO_DATA_FOLDER=$REPO_DATA_FOLDER" >> $CONFIG
+echo "PIPELINE_LOG_DIR=$PIPELINE_LOG_DIR" >> $CONFIG
+echo "PREP_LOG=$PREP_LOG" >> $CONFIG
+echo "SUPPORT_SCRIPTS=$SUPPORT_SCRIPTS" >> $CONFIG
+echo "CENSOR_FILES=$CENSOR_FILES" >> $CONFIG
+echo "CONFIG=$CONFIG" >> $CONFIG
---
+++ data_prep/support_scripts/stage_1/clean_rds_pull_scandata.r
@@ -0,0 +1,72 @@
+
+# clean_rds.r
+# Created: 6/15/20
+# Updated:
+
+# Written by Nikhil Goyal, National Institute of Mental Health, 2019-2020
+# This script does the following:
+#   1.  Keeps only baseline scans for subjects
+#   2.  Converts subject naming from NDAR_INVxxxxxxxx to sub-NDARINVxxxxxxxx
+#   3.  Exports some motion data needed in prep_stage_1 (saves to /data/stage_1/scan_data.txt)
+
+library(dplyr)
+
+mode <- function(x) {
+    ux <- unique(x)
+    ux[which.max(tabulate(match(x, ux)))]
+}
+
+fix_name <- function(x) {
+    nx <- sub("NDAR_INV","sub-NDARINV",x)
+}
+
+args <- commandArgs()
+rds_path <- args[1]
+sub_path <- args[2]
+sm_path <- args[3]
+out_path <- args[4]
+
+subject_list <- readLines(sub_path)
+subject_list <- factor(subject_list)
+sm_list <- readLines(sm_path)
+scan_sm_list <- list("subjectid","iqc_t1_good_ser","iqc_rsfmri_good_ser","iqc_rsfmri_total_ser")
+
+# Load original RDS file
+nda <- readRDS(rds_path)
+nda1 <- nda
+# Sort by interview date, keep only baseline measurements (based on date)
+nda1$interview_date <- as.Date(nda$interview_date, "%m/%d/%Y")
+nda2 <- nda1[order(as.integer(nda1$interview_date),decreasing = FALSE), ]
+# Drop the follow up records, keeping only first instance
+nda3 <- nda2 %>% distinct(subjectid, .keep_all = TRUE)
+
+# Now correct the naming scheme
+sub_ids_orig <- list(nda3$subjectid)
+sub_ids_new <- lapply(sub_ids_orig, fix_name)
+# Now replace the existing subid column with this new one
+nda3[["subjectid"]] <- sub_ids_new[[1]]
+
+# Now remove all subjects who are missing their .mat files (specific by file /data/stage_1/subs_with_motion.txt)
+# Format of strings in these files are sub-NDARINVxxxxxxxx
+# Drop subjects not in our list
+nda4 <- nda3[nda3$subjectid %in% subject_list,]
+
+# Finally, remove any completely empty rows which may have been introduced
+nda5 <- nda4[rowSums(is.na(nda4)) != ncol(nda4),]
+
+# Pull out scan data
+nda_scan <- nda5[ , (names(nda5) %in% scan_sm_list)]
+
+# Now keep subject measures of interest
+# nda6 <- nda5[ , (names(nda5) %in% sm_list)]
+
+# Save updated RDS for later use
+saveRDS(nda5, paste(out_path,"nda2.0.1_stage_1.Rds",sep="/"))
+
+# Save scan data
+write.table(nda_scan,
+            file = paste(out_path,"scan_data.txt",sep="/"),
+            sep  = ",",
+            row.names = FALSE,
+            col.names = TRUE,
+            quote = FALSE)
