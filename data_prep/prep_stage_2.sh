b03622eb0a2d7507af6b122446330220acc81946
ngoyal95 <nikhil.r.goyal@gmail.com>
2020-06-16 23:21:42 -0400

    switching to a swarm for the post-censor length analysis (single comp too slow)

--- data_prep/prep_stage_2.sh
+++ data_prep/prep_stage_2.sh
@@ -56,6 +56,7 @@ fi
 
 DATAFOLDER=$DATA_PREP/data/stage_2/scan_length_analyze_classify/
 ICAFIX_CMDS=$DATA_PREP/data/stage_2/icafix_cmds/
+SWARM_DIR=$DATA_PREP/data/stage_2/swarm/
 
 if [[ -d $CENSOR_FILES_CLEAN ]]; then
     rm $CENSOR_FILES_CLEAN/*.txt
@@ -75,7 +76,16 @@ else
     mkdir $ICAFIX_CMDS
 fi
 
+if [[ -d $SWARM_DIR ]]; then
+    rm $SWARM_DIR/logs/*.txt
+    rm $SWARM_DIR/*.swarm
+else
+    mkdir $SWARM_DIR
+    mkdir $SWARM_DIR/logs
+fi
+
 touch $DATA_PREP/data/stage_2/post_censor_subjects.txt
+touch $DATA_PREP/data/stage_2/dropped_post_censor_subjects.txt
 
 # STEP 1 - call scan_and_motion_analysis.py to do basic subject exclusion
 echo "$(date): Step 1 - Broad subject filtering based on scan and motion summary data."
@@ -83,16 +93,21 @@ echo "$(date) - calling scan_and_motion_analysis.py" >> $PREP_LOG
 python $SUPPORT_SCRIPTS/stage_2/scan_and_motion_analysis.py $DATA_PREP
 
 # STEP 2 - run more refined subject exclusion (elim subjects based on post-censoring total scan length)
-echo "$(date): Step 2 - Refined subject filtering based post-censoring scan length."
-echo "$(date) - calling scan_length_analyze_classify.sh" >> $PREP_LOG
+echo "$(date): Step 2 - Generating swarm commands for post-censor length analysis."
+echo "$(date) - generating swarm commands for scan_length_analyze_classify_single.sh" >> $PREP_LOG
 # sh $SUPPORT_SCRIPTS/stage_2/scan_length_analyze_classify.sh $ABCD_CCA_REPLICATION
-sh $SUPPORT_SCRIPTS/stage_2/scan_length_analyze_classify.sh $ABCD_CCA_REPLICATION $DATAFOLDER $ICAFIX_CMDS
+# sh $SUPPORT_SCRIPTS/stage_2/scan_length_analyze_classify.sh $ABCD_CCA_REPLICATION $DATAFOLDER $ICAFIX_CMDS
 
+# Generate swarm commands
+python $SUPPORT_SCRIPTS/stage_2/stage_2_swarm_gen.py $STAGE_2_OUT/scan_and_motion_subjects.txt $ABCD_CCA_REPLICATION $SWARM_DIR "$SUPPORT_SCRIPTS/stage_2/scan_length_analyze_classify_single.sh"
+echo "$(date): Swarm commands generated"
+echo "run swarm with command:"
+echo "\t swarm -f $SWARM_DIR/stage_2.swarm -b 50 --logdir $SWARM_DIR/logs --job-name stage_2"
 
 # number subjects remaining post-censor
-NUM_SUBS=$(wc -l $STAGE_2_OUT/post_censor_subjects.txt)
-echo "$(date): number of subjects remaining after post-censor filtering is $NUM_SUBS"
-echo "$(date) - number of subjects remaining after post-censor filtering: $NUM_SUBS" >> $PREP_LOG
+# # NUM_SUBS=$(cat $STAGE_2_OUT/post_censor_subjects.txt | wc -l)
+# echo "$(date): number of subjects remaining after post-censor filtering is $NUM_SUBS"
+# echo "$(date) - number of subjects remaining after post-censor filtering: $NUM_SUBS" >> $PREP_LOG
 
 echo "$(date) - STOP" >> $PREP_LOG
 echo "--- END STAGE 2 LOG ---" >> $PREP_LOG
---
+++ data_prep/support_scripts/stage_2/stage_2_swarm_gen.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+# stage_2_swarm_gen.py
+# Created: 6/16/20
+# Last edited:
+# Written by Nikhil Goyal, National Institute of Mental Health, 2019-2020
+
+import os
+import sys
+
+fp_sub_list = sys.argv[1]               # absolute path to file that contains subject ids
+abcd_cca_replication = sys.argv[2]      # absolute path to main directory in repo (where pipeline.config located)
+swarm_dir = sys.argv[3]                 # where to output swarm file
+script_to_call = sys.argv[4]            # name of the script to call (absolute path)
+
+subjects = [line.rstrip('\n') for line in open(fp_sub_list)]
+
+fp = os.path.join(swarm_dir,'stage_2.swarm')
+f_swarm = open(fp, 'w')
+
+for subject in subjects:
+
+    cmd = "module load fsl && {} {} {}".format(script_to_call,subject,abcd_cca_replication)
+    f_swarm.write(cmd+'\n')
+f_swarm.close()
