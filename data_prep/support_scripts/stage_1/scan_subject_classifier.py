fd0008090386f1f48eaed3aaf0851658965d67f0
ngoyal95 <nikhil.r.goyal@gmail.com>
2020-06-19 15:51:03 -0400

    stage 1 swarm gen testing

---
+++ data_prep/support_scripts/stage_1/scan_subject_classifier.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+
+# motion_scan_subject_classifier.py
+# Created: 6/19/20 (pipeline_version_1.1)
+# Last edited:
+# Written by Nikhil Goyal, National Institute of Mental Health, 2019-2020
+
+# Inputs
+#   1.  absolute path to subject pre-censor scan lengths
+#   2.  absolute path to subject post-censor scan lengths
+#   3.  absolute path to output file
+#   4.  minimum TPS number
+
+# To use this script for different FD threshold levels, you simply pass it the input path to the post-censor 0.3mm length file, and specify its output file as /classifiers/0.3mm/sub-NDARINVxxxxxxxx.txt (and similar for 0.2mm threshold) 
+
+import sys
+import os
+import numpy as np
+import pandas as pd
+import datetime
+
+pre_censor_lengths_fp   =   sys.argv[1]
+post_censor_lengths_fp  =   sys.argv[2]
+output_fp               =   sys.argv[3]
+min_tps                 =   sys.argv[4]
+
+# load pre- and post-censor scan lengths
+pre_censor_lengths = [line.rstrip('\n') for line in open(pre_censor_lengths_fp)]
+pre_censor_lengths = [int(i) for i in pre_censor_lengths]
+post_censor_lengths = [line.rstrip('\n') for line in open(post_censor_lengths_fp)]
+post_censor_lengths = [int(i) for i in post_censor_lengths]
+
+classifier=[]
+total_post_censor_len=0
+for pre_len,post_len in zip(pre_censor_lengths,post_censor_lengths):
+    if pre_len >= min_tps:
+        total_post_censor_len+=post_len
+        classifier.append(1)
+    else:
+        classifier.append(0)
+
+# Write the classifier data to file
+with open(output_fp, "w") as output:
+    for item in classifier:
+        output.write('%s\n' % item)
+
+# Return code to parent shell script for decision making
+if total_post_censor_len >= 750:
+    # subject can proceed in pipeline
+    sys.exit(1)
+elif total_post_censor_len < 750:
+    # drop subject
+    sys.exit(2)
+else:
+    # something went wrong, throw an error and store this subject id
+    sys.exit(0)
---
+++ data_prep/support_scripts/stage_1/subject_classifier.sh
@@ -0,0 +1,137 @@
+#! /bin/bash
+
+# subject_classifier.sh
+# Created: 6/15/20
+# Updated: 6/19/20 (pipeline_version_1.1)
+
+# Written by Nikhil Goyal, National Institute of Mental Health, 2019-2020
+
+# Example usage:
+# ./subject_classifier.sh <subjectid> </abs/path/to/abcd_cca_replication/>
+
+set -x
+
+# Check for config
+sub=$1
+ABCD_CCA_REPLICATION=$2
+
+if [[ -f $ABCD_CCA_REPLICATION/pipeline.config ]]; then
+    # config exists, so run it
+    # This will load BIDS_PATH, DERIVATIVES_PATH, DATA_PREP variables
+    . $ABCD_CCA_REPLICATION/pipeline.config
+else
+    echo "$ABCD_CCA_REPLICATION/pipeline.config does not exist! Please run create_config.sh."
+    exit 1
+fi
+
+# Define some paths we need
+CLASSIFIERS=$DATA_PREP/data/stage_1/classifiers/
+ICAFIX=$DATA_PREP/data/stage_1/icafix_cmds/
+pre_censor_lens=$PRE_CENSOR_LENGTHS/${sub}.txt
+
+# STEP 1 - 0.2mm FD
+post_censor_lens=$CENSOR_FILES/$sub/good_TRs_0.2mm.censor.txt
+python $SUPPORT_SCRIPTS/stage_1/scan_subject_classifier.py $pre_censor_lens $post_censor_lens $CLASSIFIERS/0.2mm/$sub.txt $MIN_TPS
+result=$?
+
+# Check that the subject has enough total time based on returned "result"
+if [[ $result -eq 2 ]]; then
+    # Subject dropped from study, note this
+    # echo "Dropping subject $sub, has less than 600 seconds of scan time post-censoring."
+    echo $sub >> $DATA_PREP/data/stage_1/subjects_drop_0.2mm.txt
+elif [[ $result -eq 1 ]]; then
+    # subject still eligible, create its task-rest0 string for ICA+FIX cmd
+    # add subject to final list of subjects
+    echo $sub >> $DATA_PREP/data/stage_1/subjects_keep_0.2mm.txt
+
+    # Now create their cmd
+    # Keep track of which scan we're looking at
+    count=1 
+    # total timepoints for valid scans for a subject
+    cmd_str=""
+    while read classification
+    do
+        if [[ $classification -eq 1 ]]; then
+            # scan flagged as keep
+            if [[ $count -eq 1 ]]; then
+                # Prepend a 0, remove @ at beginning
+                cmd_str=task-rest0$count/task-rest0$count.nii.gz
+            elif [[ $count -lt 10 ]]; then
+                # Prepend 0 since < 10, but include the @ at beginning
+                cmd_str=$cmd_str@task-rest0$count/task-rest0$count.nii.gz
+            else
+                # No need to prepend 0 since number is 10 or greater
+                cmd_str=$cmd_str@task-rest$count/task-rest$count.nii.gz
+            fi
+        else
+            # Not using this scan
+            :
+        fi
+        ((count++))
+    done < $CLASSIFIERS/0.2mm/$sub.txt
+    # echo the ICA+FIX cmd to file
+    echo $cmd_str >> $ICAFIX/$sub.txt
+
+elif [[ $result -eq 0 ]]; then
+    # Error handling
+    # echo "Something went wrong when processing $sub in script classify_scans_get_lens_clean_censors.py. Omitting subject."
+    echo $sub >> $DATA_PREP/data/stage_1/subjects_error_0.2mm.txt
+else
+    # Something else went wrong (maybe not needed?)
+    echo $sub >> $DATA_PREP/data/stage_1/subjects_keep_0.2mm.txt
+fi
+
+
+
+# STEP 2 - 0.3mm FD
+good_TRs=$CENSOR_FILES/$sub/good_TRs_0.3mm.censor.txt
+python $SUPPORT_SCRIPTS/stage_1/scan_subject_classifier.py $pre_censor_lens $post_censor_lens $CLASSIFIERS/0.3mm/$sub.txt $MIN_TPS
+result=$?
+
+# Check that the subject has enough total time based on returned "result"
+if [[ $result -eq 2 ]]; then
+    # Subject dropped from study, note this
+    # echo "Dropping subject $sub, has less than 600 seconds of scan time post-censoring."
+    echo $sub >> $DATA_PREP/data/stage_1/subjects_drop_0.3mm.txt
+elif [[ $result -eq 1 ]]; then
+    # subject still eligible, create its task-rest0 string for ICA+FIX cmd
+    # add subject to final list of subjects
+    echo $sub >> $DATA_PREP/data/stage_1/subjects_keep_0.3mm.txt
+
+    # Now create their cmd
+    # Keep track of which scan we're looking at
+    count=1 
+    # total timepoints for valid scans for a subject
+    cmd_str=""
+    while read classification
+    do
+        if [[ $classification -eq 1 ]]; then
+            # scan flagged as keep
+            if [[ $count -eq 1 ]]; then
+                # Prepend a 0, remove @ at beginning
+                cmd_str=task-rest0$count/task-rest0$count.nii.gz
+            elif [[ $count -lt 10 ]]; then
+                # Prepend 0 since < 10, but include the @ at beginning
+                cmd_str=$cmd_str@task-rest0$count/task-rest0$count.nii.gz
+            else
+                # No need to prepend 0 since number is 10 or greater
+                cmd_str=$cmd_str@task-rest$count/task-rest$count.nii.gz
+            fi
+        else
+            # Not using this scan
+            :
+        fi
+        ((count++))
+    done < $CLASSIFIERS/0.3mm/$sub.txt
+
+    # echo the ICA+FIX cmd to file
+    echo $cmd_str >> $ICAFIX/$sub.txt
+
+elif [[ $result -eq 0 ]]; then
+    # Error handling
+    # echo "Something went wrong when processing $sub in script classify_scans_get_lens_clean_censors.py. Omitting subject."
+    echo $sub >> $DATA_PREP/data/stage_1/subjects_error_0.3mm.txt
+else
+    # Something else went wrong (maybe not needed?)
+    echo $sub >> $DATA_PREP/data/stage_1/subjects_keep_0.3mm.txt
+fi
