959f3fcc60becd386e48e9269e4f70e96967391a
ngoyal95 <nikhil.r.goyal@gmail.com>
2020-06-20 16:57:23 -0400

    removed some old stage_2 support scripts, created scripts for stage 2 step 2

---
+++ data_prep/support_scripts/stage_2/calc_avg_motion.sh
@@ -0,0 +1,61 @@
+#! /bin/bash
+
+# calc_avg_motion.sh
+# Created: 6/20/20 (pipeline_version_1.2)
+# Updated:
+
+# Written by Nikhil Goyal, National Institute of Mental Health, 2019-2020
+
+# Example usage:
+#   ./calc_avg_motion.sh
+
+usage()
+{
+	echo "usage: calc_avg_motion.sh "
+}
+
+if (( $# < 1 ))
+then
+    usage
+	exit 1
+fi
+
+sub=$1
+ABCD_CCA_REPLICATION=$2
+
+if [[ -f $ABCD_CCA_REPLICATION/pipeline.config ]]; then
+    # config exists, so run it
+    # This will load BIDS_PATH, DERIVATIVES_PATH, DATA_PREP variables
+    . $ABCD_CCA_REPLICATION/pipeline.config
+else
+    echo "$ABCD_CCA_REPLICATION/pipeline.config does not exist! Please run create_config.sh."
+    exit 1
+fi
+
+# detect files of format:
+# sub-NDARINVxxxxxxxx_ses-baselineYear1Arm1_task-rest_run-<1,2,3...>_motion.tsv
+paths=`find $DERIVATIVES_PATH/$sub/ses-baselineYear1Arm1/ -maxdepth 2 -type f -name "sub-*ses-baselineYear1Arm1_task-rest*motion.tsv" ! -name "*desc-filtered*" 2> /dev/null`
+
+# check if path variable is an empty line (nothing except a newline terminator)
+if [ -z "$paths" ]; then
+    # Skip this subject
+    echo $sub >> $DATA_PREP/data/stage_2/subjects_missing_motion.txt
+else
+    num_tsv_files=$(echo "$paths" | wc -l)
+    len_classifier=$(cat $2 | wc -l)
+    # check for mis-match between the length of classifier file and number of motion.tsv files
+    if [ $num_tsv_files -eq $len_classifier ]; then
+        # correct number of tsv files for number of runs
+
+        # Save the filepaths for .tsv files to the $stage_2_out directory
+        echo "$paths" > $STAGE_2_OUT/motion_data/${sub}_tsv_paths.txt
+
+        # Now call python script to calc
+        python $SUPPORT_SCRIPTS/stage_2/subject_motion_to_meanFD.py $sub $DATA_PREP/data/stage_1/classifiers/0.3mm/$sub.txt $STAGE_2_OUT/motion_data/${sub}_tsv_paths.txt $STAGE_2_OUT/subs_motion_values.txt
+
+    else
+        # Error, skip this subject
+        echo "ERROR: subject $sub mismatch between number of rsfMRI runs ($len_classifier) and number of motion.tsv files ($num_files)."
+        exit
+    fi
+fi
---
+++ data_prep/support_scripts/stage_2/subject_motion_to_meanFD.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+# subject_motion_to_meanFD.py
+# Created: 6/16/20
+# Last edited:
+# Written by Nikhil Goyal, National Institute of Mental Health, 2019-2020
+
+import pandas as pd
+import numpy as np
+import sys
+
+def Average(lst):
+    return sum(lst) / len(lst)
+
+def calc_fd(file):
+    mot = pd.read_csv(file, sep='\t+', engine='python')
+    derv_col = ['XDt', 'YDt', 'ZDt', 'RotXDt', 'RotYDt', 'RotZDt']
+    derv = mot[derv_col].abs()
+    motion_val = round(np.mean(derv.mean())/len(derv_col),6)
+    return motion_val
+
+sub_id                  =   sys.argv[1]
+scan_classifier         =   sys.argv[2]
+subject_tsv_files       =   sys.argv[3]
+outfile                 =   sys.argv[4]
+
+classifier = [line.rstrip('\n') for line in open(scan_classifier)]
+classifier = [int(i) for i in classifier]
+tsv_files = [line.rstrip('\n') for line in open(subject_tsv_files)]
+
+motion_vals=[]
+for scan_class,tsv in zip(classifier,tsv_files):
+    if scan_class == 1:
+        scan_fd = calc_fd(tsv)
+        motion_vals.append(scan_fd)
+    else:
+        continue
+
+with open(outfile, "a") as output:
+    output.write('%s,%s\n' % sub,motion_val)
