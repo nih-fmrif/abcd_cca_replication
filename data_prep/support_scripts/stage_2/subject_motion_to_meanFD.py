1048151bc45db545cfe39cc065f9ba6a6d8e9360
ngoyal95 <nikhil.r.goyal@gmail.com>
2020-06-22 09:17:22 -0400

    testing mean FD calc with new code to exclude dropped frames

--- data_prep/support_scripts/stage_2/subject_motion_to_meanFD.py
+++ data_prep/support_scripts/stage_2/subject_motion_to_meanFD.py
@@ -12,31 +12,38 @@ import sys
 def Average(lst):
     return sum(lst) / len(lst)
 
-def calc_fd(file):
-    mot = pd.read_csv(file, sep='\t+', engine='python')
+def calc_fd(motion_list,censor_path):
+    mot     =   pd.read_csv(motion_list, sep='\t+', engine='python')
+    censor  =   [line.rstrip('\n') for line in open(censor_path)]
+    censor  =   [int(i) for i in censor]
+    # Get indices of the 0s in the censor (1 = include frame, 0 = exclude frame)
+    drop_idxs   =   [i for i, x in enumerate(censor) if x == 0]
     derv_col = ['XDt', 'YDt', 'ZDt', 'RotXDt', 'RotYDt', 'RotZDt']
     derv = mot[derv_col].abs()
+    derv = derv.drop(derv.index[drop_idxs])
     motion_val = round(np.mean(derv.mean())/len(derv_col),6)
     return motion_val
 
-sub                 =   sys.argv[1]
-scan_classifier     =   sys.argv[2]
-subject_tsv_files   =   sys.argv[3]
-outfile             =   sys.argv[4]
+sub                     =   sys.argv[1]
+scan_classifier         =   sys.argv[2]
+subject_tsv_files       =   sys.argv[3]
+subject_censor_files    =   sys.argv[4]
+outfile                 =   sys.argv[5]
 
-classifier = [line.rstrip('\n') for line in open(scan_classifier)]
-classifier = [int(i) for i in classifier]
-tsv_files = [line.rstrip('\n') for line in open(subject_tsv_files)]
+classifier      =   [line.rstrip('\n') for line in open(scan_classifier)]
+classifier      =   [int(i) for i in classifier]
+tsv_files       =   [line.rstrip('\n') for line in open(subject_tsv_files)]
+censor_files    =   [line.rstrip('\n') for line in open(subject_censor_files)]
 
 motion_vals=[]
-for scan_class,tsv in zip(classifier,tsv_files):
+for scan_class,tsv,censor in zip(classifier,tsv_files,censor):
     if scan_class == 1:
-        scan_fd = calc_fd(tsv)
+        scan_fd = calc_fd(tsv,censor)
         motion_vals.append(scan_fd)
     else:
         continue
 
 avg_fd = Average(motion_vals)
 
-with open(outfile, "a") as output:
+with open(outfile, "w") as output:
     output.write('{},{}\n'.format(sub, avg_fd))
