stage 2 updated, corrected the FD calculation

data_prep/support_scripts/stage_2/subject_motion_to_meanFD.py
@@ -2,7 +2,8 @@
 
 # subject_motion_to_meanFD.py
 # Created: 6/16/20
-# Last edited:
+# Last edited: 6/22/20 (pipeline_version_1.3)
+#   Updated how FD is calculated
 # Written by Nikhil Goyal, National Institute of Mental Health, 2019-2020
 
 import pandas as pd
@@ -13,32 +14,25 @@ def Average(lst):
     return sum(lst) / len(lst)
 
 def calc_fd(motion_list,censor_path):
-    mot     =   pd.read_csv(motion_list, sep='\t+', engine='python')
-    censor  =   [line.rstrip('\n') for line in open(censor_path)]
-    censor  =   [int(i) for i in censor]
-    # Get indices of the 0s in the censor (1 = include frame, 0 = exclude frame)
-    drop_idxs   =   [i for i, x in enumerate(censor) if x == 0]
+    mot = pd.read_csv(motion_list, sep='\t+', engine='python')
     derv_col = ['XDt', 'YDt', 'ZDt', 'RotXDt', 'RotYDt', 'RotZDt']
     derv = mot[derv_col].abs()
-    derv = derv.drop(derv.index[drop_idxs])
-    motion_val = round(np.mean(derv.mean())/len(derv_col),6)
+    motion_val = derv.sum(axis=1).mean()
     return motion_val
 
 sub                     =   sys.argv[1]
 scan_classifier         =   sys.argv[2]
 subject_tsv_files       =   sys.argv[3]
-subject_censor_files    =   sys.argv[4]
-outfile                 =   sys.argv[5]
+outfile                 =   sys.argv[4]
 
 classifier      =   [line.rstrip('\n') for line in open(scan_classifier)]
 classifier      =   [int(i) for i in classifier]
 tsv_files       =   [line.rstrip('\n') for line in open(subject_tsv_files)]
-censor_files    =   [line.rstrip('\n') for line in open(subject_censor_files)]
 
 motion_vals=[]
-for scan_class,tsv,censor in zip(classifier, tsv_files, censor_files):
+for scan_class,tsv in zip(classifier, tsv_files):
     if scan_class == 1:
-        scan_fd = calc_fd(tsv,censor)
+        scan_fd = calc_fd(tsv)
         motion_vals.append(scan_fd)
     else:
         continue