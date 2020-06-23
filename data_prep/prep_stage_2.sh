47007947ce3646389f4a34dcbcce9f04a185818f
ngoyal95 <nikhil.r.goyal@gmail.com>
2020-06-23 00:43:28 -0400

    stage 2 testing

--- data_prep/prep_stage_2.sh
+++ data_prep/prep_stage_2.sh
@@ -1,28 +1,13 @@
 #! /bin/bash
 
 # prep_stage_2.sh
-# Created: 6/16/20
-# Updated: 6/20/20 (pipeline_version_1.2)
+# Created: 6/12/20
+# Updated: 6/22/20 (pipeline_version_1.4)
 
 # Written by Nikhil Goyal, National Institute of Mental Health, 2019-2020
 
 # Expected tools on PATH:
 # R
-
-# Example usage:
-#   ./prep_stage_2.sh
-
-# usage()
-# {
-# 	echo "usage: prep_stage_2.sh <path/to/nda2.0.1.Rds/>"
-#     echo "NOTE you must provide the ABSOLUTE PATH to the NDA RDS file nda2.0.1.Rds (or whichever version is being used)"
-# }
-
-# if (( $# < 1 ))
-# then
-#     usage
-# 	exit 1
-# fi
 
 # Check for R
 Rscript_exec=$(which Rscript)
@@ -43,18 +28,11 @@ else
 fi
 
 # Check if the following folders/files exist
-STAGE_2_OUT=$DATA_PREP/data/stage_2/
 if [[ -d $STAGE_2_OUT ]]; then
     rm $STAGE_2_OUT/*.txt
     rm $STAGE_2_OUT/*.Rds
-    rm $STAGE_2_OUT/motion_data/*.txt
-    rm $STAGE_2_OUT/swarm_logs/*.{e,o}
-    rm $STAGE_2_OUT/subs_missing_motion_data/*
 else
     mkdir $STAGE_2_OUT
-    mkdir $STAGE_2_OUT/motion_data/
-    mkdir $STAGE_2_OUT/swarm_logs/
-    mkdir $STAGE_2_OUT/subs_missing_motion_data/
 fi
 
 echo "--- PREP_STAGE_2 ---"
@@ -63,10 +41,21 @@ echo "$(date) - START"
 echo "--- STAGE 2 LOG ---" >> $PREP_LOG
 echo "$(date) - START" >> $PREP_LOG
 
-# STEP 0 - determine which subjects can proceed from stage 1 (based on data/stage_1/subjects_classified/keep/sub*0.3mm)
-ls $DATA_PREP/data/stage_1/subjects_classified/keep/sub*_0.3mm | sed 's|_0.3mm||' | sed "s|${DATA_PREP}/data/stage_1/subjects_classified/keep/||" >> $DATA_PREP/data/stage_1/subjects_keep_0.3mm.txt
-# ls $DATA_PREP/data/stage_1/subjects_classified/keep/sub*_0.2mm | sed 's|0.2mm||' >> $DATA_PREP/data/stage_2/subjects_keep_0.2mm.txt
-stage_1_subjects=$DATA_PREP/data/stage_1/subjects_keep_0.3mm.txt
+# STEP 0 - Get list of subjects which can proceed from stage 1, get aggregated list of motion
+ls $STAGE_1_OUT/subjects_classified/$FD_THRESH/$SCAN_FD_THRESH_1/keep/sub-* | sort | sed "s|${STAGE_1_OUT}/subjects_classified/${FD_THRESH}/${SCAN_FD_THRESH_1}/keep||" >> $STAGE_2_OUT/motion_filt_subjects.txt
+
+subjects=$STAGE_2_OUT/motion_filt_subjects.txt
+
+NUM_SUBS_STAGE_1=$(cat $subjects | wc -l)
+echo "$(date) - Number subjects which passed pre- and post-censor scan length, and motion requirements from stage 1 (FD threshold ${FD_THRESH}mm, scan-level motion threshold ${SCAN_FD_THRESH_1}mm): $NUM_SUBS_STAGE_1" >> $PREP_LOG
+
+# Aggregate the motion data for the subjects that passed filtering in stage 1
+motion_file=$STAGE_2_OUT/subjects_mean_fds.txt
+touch $motion_file
+echo "subjectid,mean_fd" >> $motion_file
+while read subject; do
+    cat $STAGE_1_OUT/subject_mean_fd/$FD_THRESH/$SCAN_FD_THRESH_1/$subject.txt >> $motion_file
+done < $subjects
 
 # STEP 1 - Call R script to clean RDS, pull scan data, filter subjects missing scandata or not meeting QC/PC requirement for T1w scans
 echo "$(date) - STEP 1 - calling RDS cleaning script" >> $PREP_LOG
@@ -83,16 +72,19 @@ echo "$(date) - Number subjects dropped due to missing scan data or not meeting 
 echo "$(date) - Number subjects after RDS cleaning: $NUM_SUBS_RDS"
 echo "$(date) - Number subjects after RDS cleaning: $NUM_SUBS_RDS" >> $PREP_LOG
 
-# STEP 2 - Calculate average motion for subjects in $STAGE_2_OUT/prep_stage_2_final_subjects.txt
-echo "$(date) - STEP 2 - Generating swarm to calculate average motion across valid runs for each subject (calling stage_2_swarm_gen.py)" >> $PREP_LOG
-echo "$(date) - STEP 2 - Generating swarm to calculate average motion across valid runs for each subject (calling stage_2_swarm_gen.py)"
-python $SUPPORT_SCRIPTS/stage_2/stage_2_swarm_gen.py $STAGE_2_OUT/prep_stage_2_rds_subjects.txt $ABCD_CCA_REPLICATION $STAGE_2_OUT $SUPPORT_SCRIPTS/stage_2/calc_mean_fd.sh
+# STEP 2 - Call final RDS proc script, extract final subjects and SMs
+echo "$(date) - STEP 2 - Call final RDS proc script, extract final subjects and SMs" >> $PREP_LOG
+echo "$(date) - STEP 2 - Call final RDS proc script, extract final subjects and SMs"
+Rscript $SUPPORT_SCRIPTS/stage_2/final_rds_proc.r $DATA_PREP/data/stage_2/nda2.0.1_stage_2.Rds $stage_2_subjects $DATA_PREP/data/subject_measures.txt $DATA_PREP/data/ica_subject_measures.txt $STAGE_2_OUT
+NUM_SUBS=$(cat $STAGE_2_OUT/final_subjects.txt | wc -l)
+echo "Final number of subjects: $NUM_SUBS"
+NUM_SMS=$(cat $STAGE_2_OUT/final_subject_measures.txt | wc -l)
+echo "Final number of subjects: $NUM_SMS"
 
-echo "$(date) - swarm file created, call with the following commands. MAKE SURE TO ACTIVATE ABCD_CCA_REPLICATION CONDA ENVIRONMENT PRIOR TO RUNNING!"
-echo "          swarm -f $STAGE_2_OUT/stage_2.swarm -b 500 --logdir $STAGE_2_OUT/swarm_logs/ --time=00:01:00 --job-name stage_2"
-
-echo "$(date) - swarm file created, call with the following commands. MAKE SURE TO ACTIVATE ABCD_CCA_REPLICATION CONDA ENVIRONMENT PRIOR TO RUNNING!" >> $PREP_LOG
-echo "          swarm -f $STAGE_2_OUT/stage_2.swarm -b 500 --logdir $STAGE_2_OUT/swarm_logs/ --time=00:01:00 --job-name stage_2" >> $PREP_LOG
+# STEP 3 - Generate final VARS.txt matrix
+echo "$(date) - STEP 3 - Generating final VARS.txt matrix (abcd_cca_replication/data/VARS.txt)" >> $PREP_LOG
+echo "$(date) - STEP 3 - Generating final VARS.txt matrix (abcd_cca_replication/data/VARS.txt)"
+python $SUPPORT_SCRIPTS/stage_2/vars.py $STAGE_2_OUT/final_subjects.txt $STAGE_2_OUT/final_subject_measures.txt $motion_file $STAGE_2_OUT/VARS_no_motion.txt $ABCD_CCA_REPLICATION/data/VARS.txt
 
 echo "$(date) - STOP" >> $PREP_LOG
 echo "--- END STAGE 2 LOG ---" >> $PREP_LOG
