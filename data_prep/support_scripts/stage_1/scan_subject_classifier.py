65cafffb60245f07050b10a059c19499f6d41181
ngoyal95 <nikhil.r.goyal@gmail.com>
2020-06-24 13:53:13 -0400

    added additional code to produce concatenated censor files for subjects

--- data_prep/support_scripts/stage_1/scan_subject_classifier.py
+++ data_prep/support_scripts/stage_1/scan_subject_classifier.py
@@ -37,18 +37,22 @@ sub                     =   sys.argv[1]
 pre_censor_lengths_fp   =   sys.argv[2]
 post_censor_lengths_fp  =   sys.argv[3]
 tsv_files_fp            =   sys.argv[4]
-classifier_output_fp    =   sys.argv[5]
-subject_mean_fd_outp_fp =   sys.argv[6]
-min_tps                 =   int(sys.argv[7])
-scan_fd_thresh          =   float(sys.argv[8])
+censor_files_fp         =   sys.argv[5]
+classifier_output_fp    =   sys.argv[6]
+subject_mean_fd_out_fp  =   sys.argv[7]
+concat_censor_out_fp    =   sys.argv[8]
+min_tps                 =   int(sys.argv[9])
+scan_fd_thresh          =   float(sys.argv[10])
 
 # load pre- and post-censor scan lengths
-pre_censor_lengths = [line.rstrip('\n') for line in open(pre_censor_lengths_fp)]
-pre_censor_lengths = [int(i) for i in pre_censor_lengths]
-post_censor_lengths = [line.rstrip('\n') for line in open(post_censor_lengths_fp)]
-post_censor_lengths = [int(i) for i in post_censor_lengths]
-tsv_files   =   [line.rstrip('\n') for line in open(tsv_files_fp)]
+pre_censor_lengths  =   [line.rstrip('\n') for line in open(pre_censor_lengths_fp)]
+pre_censor_lengths  =   [int(i) for i in pre_censor_lengths]
+post_censor_lengths =   [line.rstrip('\n') for line in open(post_censor_lengths_fp)]
+post_censor_lengths =   [int(i) for i in post_censor_lengths]
+tsv_files           =   [line.rstrip('\n') for line in open(tsv_files_fp)]
+censor_files        =   [line.rstrip('\n') for line in open(censor_files_fp)]
 
+agg_censor=[]
 classifier=[]
 motion_vals=[]
 total_post_censor_len=0
@@ -56,19 +60,22 @@ total_post_censor_len=0
 # Check that all three lists pre_censor_lengths, post_censor_lengths, tsv_files are same length (otherwise return 303)
 
 l1 = len(pre_censor_lengths)
-if any(len(lst) != l1 for lst in [post_censor_lengths,tsv_files]):
+if any(len(lst) != l1 for lst in [post_censor_lengths, tsv_files, censor_files]):
     # ERROR, not all the same length
     sys.exit(303)
 
-for pre_len,post_len,tsv in zip(pre_censor_lengths,post_censor_lengths,tsv_files):
+for pre_len,post_len,tsv_fp,censor_fp in zip(pre_censor_lengths, post_censor_lengths, tsv_files, censor_files):
     if pre_len >= min_tps:
-        scan_fd = calc_fd(tsv)
+        scan_fd = calc_fd(tsv_fp)
+        censor = [line.rstrip('\n') for line in open(censor_fp)]
+        censor = [int(i) for i in censor]
 
         if scan_fd <= scan_fd_thresh:
             # scan motion passes
             motion_vals.append(scan_fd)
-            classifier.append(1)
             total_post_censor_len+=post_len
+            agg_censor.extend(censor)
+            classifier.append(1)
         else:
             # scan has too much motion, drop the scan
             classifier.append(0)
@@ -83,10 +90,15 @@ with open(classifier_output_fp, "a") as output:
     for item in classifier:
         output.write('%s\n' % item)
 
+# Save the subsetted censor
+with open(concat_censor_out_fp, "w") as output:
+    for item in agg_censor:
+        output.write('%s\n' % item)
+
 # Write the average fd to file (for the given scan_fd_thresh) (if any scans passed)
 try:
     avg_fd = Average(motion_vals)
-    with open(subject_mean_fd_outp_fp, "a") as output:
+    with open(subject_mean_fd_out_fp, "a") as output:
         output.write('{},{}\n'.format(sub, avg_fd))
 except ZeroDivisionError as err:
     print("Subject had no valid scans for scan fd threshold of {}".format(scan_fd_thresh))
