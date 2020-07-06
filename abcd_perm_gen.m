06b161997284ae430e1f781d32494f2b4d6bb043
ngoyal95 <nikhil.r.goyal@gmail.com>
2020-07-06 13:43:03 -0400

    added path var so the code can be compiled

--- abcd_perm_gen.m
+++ abcd_perm_gen.m
@@ -3,24 +3,25 @@
 % Created: 7/1/2020
 % Modified: 7/6/20 (made the script into a function)
 
-function abcd_perm_gen(N_perm)
-    if nargin<1
+function abcd_perm_gen(N_perm, working_dir)
+    if nargin<2
         N_perm=100000;
+        working_dir='./'
     end
 
-    addpath(genpath('./dependencies/'));
-    addpath(genpath('./data/'));
+    addpath(genpath(sprintf('%s/dependencies/', working_dir)));
+    addpath(genpath(sprintf('%s/data/', working_dir)));
 
     % Number of permutations (100,000)
     % N_perm=100000;
 
     % --- GENERATE PERMUTATIONS ---
     % Generate permutations using the hcp2blocks package
-    blocksfile='./data/blocksfile.csv';
-    [EB,tab] = abcd2blocks('./data/VARS.txt',blocksfile, [100 10])
+    blocksfile='/data/blocksfile.csv';
+    [EB,tab] = abcd2blocks('/data/VARS.txt',blocksfile, [100 10])
     PAPset=palm_quickperms([ ], EB, N_perm); 
     % Note, PAPset is the final matrix of permuations (one permutation per column)
 
     % Now save PAPset to file
-    writematrix(PAPset,'./data/PAPset.txt');
+    writematrix(PAPset,'/data/PAPset.txt');
 end
