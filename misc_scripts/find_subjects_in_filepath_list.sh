61621fd2949c6a763fa1c31b0c48f2049af43c8d
ngoyal95 <nikhil.r.goyal@gmail.com>
2020-07-07 15:39:00 -0400

    script to pull out filepaths which contain specific subject ids

---
+++ misc_scripts/find_subjects_in_filepath_list.sh
@@ -0,0 +1,18 @@
+# Script will find the relevant lines from a file ("filepaths") that contain a subject id, then save those lines to a new file
+
+subject_list=$1
+filepaths=$2
+outdir=$3
+
+while read subject; do
+
+    line=$(cat $filepaths | grep $subject)
+
+    if [ -z "$line" ]; then
+        :
+    else
+        echo "$line" >> $outdir/subset_filepaths.txt
+    fi
+
+
+done < $subject_list
