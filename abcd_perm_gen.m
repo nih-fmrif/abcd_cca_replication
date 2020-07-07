6d44200e7913f0e00d670bf9ea32443551dc673b
ngoyal95 <nikhil.r.goyal@gmail.com>
2020-07-07 15:00:57 -0400

    added script to get N random subjects, also updated abcd_perm_gen to accept a specific subfolder in data/ so that we can look at different subsets

--- abcd_perm_gen.m
+++ abcd_perm_gen.m
@@ -2,25 +2,30 @@
 % Written by Nikhil Goyal, National Institute of Mental Health, 2019-2020
 % Created: 7/1/2020
 % Modified: 7/6/20 (made the script into a function)
+% 7/7/20 (updated the palm_quickperms call to make it faster)
 
-function abcd_perm_gen(N_perm, working_dir)
-    if nargin<2
-        print("ERROR, not enough arguments. Please input number of permutations and the path to /abcd_cca_replication/")
-        print("Example: abcd_perm_gen 1000 /data/ABCD_MBDU/goyaln2/abcd_cca_replication/")
+% n_subs = name of subset folder (e.x. 5013_subs) where the data is/should be saved
+
+% Example call:
+% abcd_perm_gen(100000, "/data/ABCD_MBDU/goyaln2/abcd_cca_replication/", "5013_subs")
+
+
+function abcd_perm_gen(N_perm, abcd_cca_dir, n_subs)
+    if nargin<3
+        print("ERROR, not enough arguments.")
+        print("Example: abcd_perm_gen(100000, '/data/ABCD_MBDU/goyaln2/abcd_cca_replication/', '5013_subs')")
         exit
     end
 
     if ~isdeployed
-        addpath(genpath(sprintf('%s/dependencies/', working_dir)));
-        addpath(genpath(sprintf('%s/data/', working_dir)));
+        addpath(genpath(sprintf('%s/dependencies/', abcd_cca_dir)));
+        addpath(genpath(sprintf('%s/data/', abcd_cca_dir)));
     end
 
-    % --- GENERATE PERMUTATIONS ---
     % Generate permutations using the hcp2blocks package
-    blocksfile = sprintf('%s/data/blocksfile.csv', working_dir);
-    % blocksfile='/data/blocksfile.csv';
-    % sprintf('%s/data/VARS.txt', working_dir)
-    [EB,tab] = abcd2blocks(sprintf('%s/data/VARS.txt', working_dir), blocksfile, [100 10])
+    in_VARS = sprintf('%s/data/%s/VARS.txt', abcd_cca_dir, n_subs);
+    blocksfile = sprintf('%s/data/%s/blocksfile.csv', abcd_cca_dir, n_subs);
+    [EB,tab] = abcd2blocks(in_VARS, blocksfile, [100 10]);
 
     % NOTE, call palm_quickperms with the following options (for fastest calculations while also finding permutations proper)
     % [Pset,VG] = palm_quickperms(M,EB,P,EE,ISE,CMCx,CMCp)
@@ -31,9 +36,9 @@ function abcd_perm_gen(N_perm, working_dir)
     % ISE = false (so we don't do sign flips)
     % CMCx = true (this only applies for design matix, so we set to true)
     % CMCp = true (allow duplicate permutations, for speed and since we have nearly infinite possible permutations)
-    PAPset=palm_quickperms([ ], EB, N_perm, true, false, true, true);
-    % Note, PAPset is the final matrix of permuations (one permutation per column)
+    Pset=palm_quickperms([ ], EB, N_perm, true, false, true, true);
+    % Note, Pset is the final matrix of permuations (one permutation per column)
 
-    % Now save PAPset to file
-    writematrix(PAPset,sprintf('%s/data/PAPset.txt', working_dir));
+    % Now save Pset to file
+    writematrix(Pset, sprintf('%s/data/%s/Pset.txt', abcd_cca_dir, n_subs));
 end
---
+++ misc_scripts/random_n_subjects.py
@@ -0,0 +1,23 @@
+# random_n_subjects.py
+# Written by Nikhil Goyal, National Institute of Mental Health, 2019-2020
+# Created: 7/7/2020
+# Modified:
+
+# Selects a random N-subject subset from the given subject list
+
+import sys
+import os
+import random
+
+sub_list = sys.argv[1]
+out_path = sys.argv[2]
+N = int(sys.argv[3])
+
+subs = [line.rstrip('\n') for line in open(sub_fp)]
+
+random.shuffle(subs)
+
+# Save the first N subjects from randomized list
+with open(out_path, "w") as output:
+    for i in range(0,N):
+        output.write('%s\n' % subs[i])
