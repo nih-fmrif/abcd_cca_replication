531a55ef5a4754461721c1a779dc9452b49ed23d
ngoyal95 <nikhil.r.goyal@gmail.com>
2020-07-09 17:41:55 -0400

    formatting

--- abcd_perm_agg.m
+++ abcd_perm_agg.m
@@ -5,7 +5,7 @@
 
 % Script is used in batch processing to calculate CCA for each of the 100,000 permutations we generate
 
-function abcd_perm_agg(perms_per_batch_in, N_perm_in, N_dim_in, abcd_cca_dir, n_subs)
+function abcd_perm_agg(perms_per_batch_in, N_perm_in, N_dim_in, abcd_cca_dir, n_subs_in)
     if nargin<5
         sprintf("ERROR, not enough arguments.")
         sprintf("Example: abcd_perm_agg(1000, 100000, 70, '/data/ABCD_MBDU/goyaln2/abcd_cca_replication/', 500)")
@@ -15,16 +15,16 @@ function abcd_perm_agg(perms_per_batch_in, N_perm_in, N_dim_in, abcd_cca_dir, n_
     if ~isdeployed
         addpath(genpath(sprintf('%s/dependencies/', abcd_cca_dir)));
         addpath(genpath(sprintf('%s/data/', abcd_cca_dir)));
-        perms_per_batch   =   perms_per_batch_in;
-        N_perm   =   N_perm_in;
-        N_dim   =   N_dim_in;
-        n_subs  =   n_subs_in;
+        perms_per_batch =   perms_per_batch_in;
+        N_perm          =   N_perm_in;
+        N_dim           =   N_dim_in;
+        n_subs          =   n_subs_in;
     elseif isdeployed
         % When compiled matlab, it reads the command line args all as strings so we need to convert
-        perms_per_batch   =   str2num(perms_per_batch_in);
-        N_perm   =   str2num(N_perm_in);
-        N_dim   =   str2num(N_dim_in);
-        n_subs  =   str2num(n_subs_in);
+        perms_per_batch =   str2num(perms_per_batch_in);
+        N_perm          =   str2num(N_perm_in);
+        N_dim           =   str2num(N_dim_in);
+        n_subs          =   str2num(n_subs_in);
     end
 
     s_agg = struct('perms',{},'r',{},'nullNETr',{},'nullNETv',{},'nullSMr',{},'nullSMv',{});
