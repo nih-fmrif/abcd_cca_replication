4d75c129f213f5ee8b72774964896eb9fdd2f3cf
ngoyal95 <nikhil.r.goyal@gmail.com>
2020-07-24 15:31:30 -0400

    syntax error

--- data_prep/support_scripts/stage_0/stage_0_swarm_gen.py
+++ data_prep/support_scripts/stage_0/stage_0_swarm_gen.py
@@ -12,7 +12,7 @@ fp_sub_list             =   sys.argv[1] # absolute path to subject list (/data/A
 script_to_call          =   sys.argv[2] # absolute path to script (/data/ABCD_MBDU/goyaln2/abcd_cca_replication/data_prep/support_scripts/stage_0/abcd_censor.py)
 FD_th                   =   sys.argv[3] # desired FD Threshold (set in config, default is 0.3mm)
 seg                     =   sys.argv[4] # number of consecutive "good" timepoints (i.e. those with FD below 0.3mm) required for this setment to be kept
-output_dir              =   sys,argv[5] # absolute path to /abcd_cca_replication/data_prep/data/stage_0/censor_files/
+output_dir              =   sys.argv[5] # absolute path to /abcd_cca_replication/data_prep/data/stage_0/censor_files/
 swarm_dir               =   sys.argv[6] # absolute path to directory where to print out the swarm file (/data/ABCD_MBDU/goyaln2/abcd_cca_replication/data_prep/data/stage_0/)
 
 subjects = [line.rstrip('\n') for line in open(fp_sub_list)]
