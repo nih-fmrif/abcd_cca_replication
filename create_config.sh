a7500dbe4b2f34299e87d8609560196b46dab988
ngoyal95 <nikhil.r.goyal@gmail.com>
2020-07-27 14:28:25 -0400

    added new variables for stage 3

--- create_config.sh
+++ create_config.sh
@@ -6,7 +6,7 @@
 
 usage()
 {
-	echo "usage: create_config.sh <path/to/main/abcd_bids/bids/> <path/to/raw/nda2.0.1.Rds>"
+	echo "usage: create_config.sh <path/to/main/abcd_bids/bids/> <path/to/raw/nda2.0.1.Rds> <path/to/reprocessed/DCAN/output/"
     echo "NOTE you must provide the ABSOLUTE PATH to the main directory of the ABCD collection 3165 download. for example: /data/ABCD/abcd_bids/bids/"
     echo "NOTE you must provide the ABSOLUTE PATH to the NDA RDS file. for example /data/ABCD_MBDU/goyaln2/abcd_cca_replication/data_prep/data/nda2.0.1.Rds"
 }
@@ -19,10 +19,7 @@ fi
 
 BIDS_PATH=$1
 NDA_RDS_RAW=$2
-MIN_TPS=190
-FD_THRESH=0.3
-SCAN_FD_THRESH_1=0.3
-SCAN_FD_THRESH_2=0.15
+DCAN_REPROC=$3
 DERIVATES_PATH=$BIDS_PATH/derivatives/abcd-hcp-pipeline/
 DATA_PREP=$PWD/data_prep/
 MAIN_REPO_DATA_FOLDER=$PWD/data/
@@ -38,6 +35,41 @@ STAGE_2_OUT=$DATA_PREP/data/stage_2/
 STAGE_3_OUT=$DATA_PREP/data/stage_3/
 STAGE_4_OUT=$DATA_PREP/data/stage_4/
 STAGE_5_OUT=$DATA_PREP/data/stage_5/
+FINAL_SUBJECTS=$STAGE_3_OUT/final_subjects.txt
+FINAL_SUBJECT_MEASURES=$STAGE_2_OUT/final_subject_measures.txt
+
+# DONT FORGET TO CHANGE THESE MANUALLY (as needed)!
+TR_INTERVAL=0.8
+MIN_TPS=190
+FD_THRESH=0.3
+SCAN_FD_THRESH_1=0.3
+SCAN_FD_THRESH_2=0.15
+
+
+# THE FOLLOWING GET SET DYANMICALLY IN
+# STAGE_3
+#   $NUMSUBS
+#   $GICA
+#   $DR
+
+if [[ -f $CONFIG ]]; then
+    read -p "A config file already exists. Are you sure you want to overwrite it? " -n 1 -r
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        # overwrite itss
+        echo "Configuration file overwritten."
+        echo
+        rm $CONFIG
+        touch $CONFIG
+    else
+        # Exit
+        echo "NOT overwriting config file."
+        exit 1
+    fi
+else
+    # config doesn't exist, so create it
+    touch $CONFIG
+fi
 
 echo
 echo "---ABCD CCA Pipeline Config Setup---"
@@ -63,30 +95,12 @@ echo "STAGE_5_OUT=$STAGE_5_OUT"
 
 echo
 echo "--VARIABLES--"
+echo "TR_INTERVAL=$TR_INTERVAL"
 echo "MIN_TPS=$MIN_TPS"
 echo "FD_THRESH=$FD_THRESH"
 echo "SCAN_FD_THRESH_1=$SCAN_FD_THRESH_1"
 echo "SCAN_FD_THRESH_2=$SCAN_FD_THRESH_2"
 echo "------------------------------------"
-
-if [[ -f $CONFIG ]]; then
-    read -p "A config file already exists. Are you sure you want to overwrite it? " -n 1 -r
-    echo
-    if [[ $REPLY =~ ^[Yy]$ ]]; then
-        # overwrite it
-        echo "Configuration file overwritten."
-        echo
-        rm $CONFIG
-        touch $CONFIG
-    else
-        # Exit
-        echo "NOT overwriting config file."
-        exit 1
-    fi
-else
-    # config doesn't exist, so create it
-    touch $CONFIG
-fi
 
 # Now write these variables
 echo "BIDS_PATH=$BIDS_PATH" >> $CONFIG
@@ -108,6 +122,7 @@ echo "STAGE_3_OUT=$STAGE_3_OUT" >> $CONFIG
 echo "STAGE_4_OUT=$STAGE_4_OUT" >> $CONFIG
 echo "STAGE_5_OUT=$STAGE_5_OUT" >> $CONFIG
 
+echo "TR_INTERVAL=$TR_INTERVAL" >> $CONFIG
 echo "MIN_TPS=$MIN_TPS" >> $CONFIG
 echo "FD_THRESH=$FD_THRESH" >> $CONFIG
 echo "SCAN_FD_THRESH_1=$SCAN_FD_THRESH_1" >> $CONFIG
