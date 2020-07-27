8e2805d7852e263534f64e94d8ec5c3f26cbd3f4
ngoyal95 <nikhil.r.goyal@gmail.com>
2020-07-27 14:54:01 -0400

    fixed some paths

--- data_prep/prep_stage_3.sh
+++ data_prep/prep_stage_3.sh
@@ -27,6 +27,8 @@ fi
 if [[ -d $STAGE_3_OUT ]]; then
     rm $STAGE_3_OUT/*.txt
     rm $STAGE_3_OUT/*.Rds
+    rm $STAGE_3_OUT/swarm_logs/icafix/*.{e,o}
+    rm $STAGE_3_OUT/swarm_logs/censor_and_truncate/*.{e,o}
     :
 else
     mkdir -p $STAGE_3_OUT
@@ -39,12 +41,12 @@ echo "--- PREP_STAGE_3 ---"
 echo "PREP STAGE 3 Requires a number of steps to be performed manually. A number of scripts will be run to generate batch commands (designed for the NIH Biowulf) along with instructions on how to use the commands."
 echo "If you are not using the NIH Biowulf, you will need to adapt these commands to your own HPC."
 
-echo "STEP 1: ICA+FIX"
-echo "For the ICA+FIX runs, we recommend using our included fix_multi_run.sh script, with ICA+FIX 1.06.15 and HPC pipeline 4.1.3"
-echo "You will need to properly configure the ICA+FIX settings.sh file for your system."
-echo "Example ICA+FIX command: "
+echo "- STEP 1: ICA+FIX -"
+echo "- For the ICA+FIX runs, we recommend using our included fix_multi_run.sh script, with ICA+FIX 1.06.15 and HPC pipeline 4.1.3"
+echo "- You will need to properly configure the ICA+FIX settings.sh file for your system."
+echo "- Example ICA+FIX command: "
 echo "  cd /path/to/subject/folder/MNINonLinear/Results/ /path/to/fix_multi_run.sh task-rest01/task-rest01.nii.gz@task-rest02/task-rest02.nii.gz 2000 fix_proc/task-rest_concat TRUE"
-echo "NOTE, if you want to change the SWARM commands, you need to manually change the code in this script."
+echo "- NOTE, if you want to change the SWARM commands, you need to manually change the code in this script."
 while read subject; do
     icafix=$(cat $STAGE_1_OUT/icafix_cmds/$FD_THRESH/$SCAN_FD_THRESH_1/$subject.txt)
     echo "export MCR_CACHE_ROOT=/lscratch/\$SLURM_JOB_ID && module load R fsl connectome-workbench && cd /data/ABCD_MBDU/abcd_bids/bids/derivatives/dcan_reproc/$subject/ses-baselineYear1Arm1/files/MNINonLinear/Results && /data/ABCD_MBDU/goyaln2/fix/fix_multi_run.sh $icafix 2000 fix_proc/task-rest_concat TRUE /data/ABCD_MBDU/goyaln2/fix/training_files/HCP_Style_Single_Multirun_Dedrift.RData" >> $STAGE_3_OUT/icafix.swarm
@@ -53,21 +55,26 @@ echo "ICA+FIX SWARM file generated! Located in $STAGE_3_OUT/icafix.swarm."
 echo "Run the swarm as follows:"
 echo "  swarm -f icafix.swarm -g 32 --gres=lscratch:50 --time 24:00:00 --logdir $STAGE_3_OUT/swarm_logs/icafix/ --job-name icafix"
 
-
+echo
 echo "STEP 2: Get final subject list (based on presence of task-rest_concat_hp2000_clean.nii.gz)"
 echo "STEP 3: Generate censor+truncate commands)"
 echo "To perform steps 2 & 3, run the script $SUPPORT_SCRIPTS/prep_stage_3_steps2and3.sh (no arguments required)."
 echo "NOTE, Step 3 will require manually submitting/running a SWARM job to do censor+truncate."
 
 
+echo
 echo "STEP 4: MELODIC Group-ICA"
 echo "Run MELODIC using the script: $SUPPORT_SCRIPTS/run_melodic.sh"
 
+echo
 echo "STEP 5: dual_regression"
-echo "Run dual_regression as follows (NOTE, on the NIH Biowulf this command will be automatically submnitted to the cluster):"
-echo " ABCD_CCA_REPLICATION="$(dirname "$PWD")" && . $ABCD_CCA_REPLICATION/pipeline.config && dual_regression && export FSL_MEM=32 && dual_regression $GICA/melodic_IC 1 -1 0 $DR `cat $FINAL_SUBJECTS`"
+echo "Run dual_regression using the script: $SUPPORT_SCRIPTS/run_dual_regression.sh"
 
+# echo "Run dual_regression as follows (NOTE, on the NIH Biowulf this command will be automatically submnitted to the cluster):"
+# echo " ABCD_CCA_REPLICATION="$(dirname "$PWD")" && . $ABCD_CCA_REPLICATION/pipeline.config && dual_regression && export FSL_MEM=32 && dual_regression $GICA/melodic_IC 1 -1 0 $DR `cat $FINAL_SUBJECTS`"
 
+echo
 echo "STEP 6: slices_summary"
-echo "Run slices_summary as follows:"
-echo "  ABCD_CCA_REPLICATION="$(dirname "$PWD")" && . $ABCD_CCA_REPLICATION/pipeline.config && slices_summary $GICA/melodic_IC 4 /usr/local/apps/fsl/6.0.1/data/standard/MNI152_T1_2mm $GICA/melodic_IC.sum -1"
+echo "Run slices_summary using the script: $SUPPORT_SCRIPTS/run_slices_summary.sh"
+
+# echo "  ABCD_CCA_REPLICATION="$(dirname "$PWD")" && . $ABCD_CCA_REPLICATION/pipeline.config && slices_summary $GICA/melodic_IC 4 /usr/local/apps/fsl/6.0.1/data/standard/MNI152_T1_2mm $GICA/melodic_IC.sum -1"
