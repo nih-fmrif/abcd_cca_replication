cea4e34e77f6cd4082f60f582756721c51baa80f
ngoyal95 <nikhil.r.goyal@gmail.com>
2021-04-07 19:03:09 -0400

    simple wrapper script make easy to run the verify

---
+++ data_prep/run_icafix_verify.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+# run_icafix_verify.sh
+# Created: 4/7/2021
+
+# EXAMPLE USAGE
+# ./run_icafix_verify.sh
+
+ABCD_CCA_REPLICATION="$(dirname "$PWD")"
+# Check for and load config
+if [[ -f $ABCD_CCA_REPLICATION/pipeline.config ]]; then
+    . $ABCD_CCA_REPLICATION/pipeline.config
+else
+    echo "$ABCD_CCA_REPLICATION/pipeline.config does not exist! Please run create_config.sh."
+    exit 1
+fi
+
+./$SUPPORT_SCRIPTS/stage_3/verify_icafix_outputs.sh $ABCD_CCA_REPLICATION
