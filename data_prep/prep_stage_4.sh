265601d154c0246250ed63be54e9ec0c76c94f91
ngoyal95 <nikhil.r.goyal@gmail.com>
2021-04-22 12:33:46 -0400

    un-commented some lines

--- data_prep/prep_stage_4.sh
+++ data_prep/prep_stage_4.sh
@@ -35,13 +35,13 @@ fi
 
 # STEP 1 - FSLNets to generate subject-level connectomes
 echo "Generating subject-level connectomes using FSLNets. This may take a while (6-8 hours)."
-# echo "gica_path=$GICA"
-# echo "dr_path=$DR"
-# matlab -nodisplay -nodesktop -nojvm -r "stage_4_out="$STAGE_4_OUT/$NUMSUBS/"; gica_path="$GICA"; dr_path="$DR";  run $SUPPORT_SCRIPTS/stage_4/abcd_netmats.m"
+echo "gica_path=$GICA"
+echo "dr_path=$DR"
+matlab -nodisplay -nodesktop -nojvm -r "stage_4_out="$STAGE_4_OUT/$NUMSUBS/"; gica_path="$GICA"; dr_path="$DR";  run $SUPPORT_SCRIPTS/stage_4/abcd_netmats.m"
 
 # STEP 2 - Generate the NET matrix
 echo "Generating the aggregated subject connectome matrix."
-# $PYTHON $SUPPORT_SCRIPTS/stage_4/NET.py $STAGE_4_OUT/$NUMSUBS/raw_netmats_001.txt 200 $NUMSUBS $STGE_3_OUT/paths_to_NIFTI_files.txt $FINAL_SUBJECTS $MAIN_REPO_DATA_FOLDER/$NUMSUBS/NET.txt
+$PYTHON $SUPPORT_SCRIPTS/stage_4/NET.py $STAGE_4_OUT/$NUMSUBS/raw_netmats_001.txt 200 $NUMSUBS $STGE_3_OUT/paths_to_NIFTI_files.txt $FINAL_SUBJECTS $MAIN_REPO_DATA_FOLDER/$NUMSUBS/NET.txt
 
 # STEP 3 - Generate VARS matrix
 echo "Generating the finalized subject measure matrix."
