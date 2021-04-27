4cf7bff97d159eb0f6d0ee41ccf03df366e99e6e
ngoyal95 <nikhil.r.goyal@gmail.com>
2021-04-27 10:06:25 -0400

    edited the matlab call

--- data_prep/prep_stage_4.sh
+++ data_prep/prep_stage_4.sh
@@ -42,10 +42,10 @@ fi
 
 # STEP 1 - FSLNets to generate subject-level connectomes
 echo "Generating subject-level connectomes using FSLNets. This may take a while (6-8 hours)."
-echo "stage_4_out=$STAGE_4_OUT/$NUMSUBS/"
-echo "gica_path=$GICA"
-echo "dr_path=$DR"
-matlab -nodisplay -nodesktop -nojvm -r "stage_4_out="$STAGE_4_OUT/$NUMSUBS/"; gica_path="$GICA"; dr_path="$DR";  run $SUPPORT_SCRIPTS/stage_4/abcd_netmats.m"
+# echo "stage_4_out=$STAGE_4_OUT/$NUMSUBS/"
+# echo "gica_path=$GICA"
+# echo "dr_path=$DR"
+matlab -nodisplay -nodesktop -nojvm -r "stage_4_out="$STAGE_4_OUT/$NUMSUBS/"; gica_path="$GICA"; dr_path="$DR"; abcd_cca_dir="$ABCD_CCA_REPLICATION"; n_subs_in="$NUMSUBS"; run $SUPPORT_SCRIPTS/stage_4/abcd_netmats.m"
 
 # STEP 2 - Generate the NET matrix
 echo "Generating the aggregated subject connectome matrix."
